c83eab5a8c2c70a94370bc19302dc1ef317cca7c
informes-web <informes@wddconsultores.com>
2025-11-27 10:45:18 -0600

    Fuentes
    Google fonts

--- functions.php
+++ functions.php
@@ -8,6 +8,9 @@ function my_theme_enqueue_styles() {
 add_action( 'wp_head', 'add_tailwind_css' );
 function add_tailwind_css() {
     ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
